2cf15d8258502f09328e4f67b01a89e2ad0a5396
Yuushaa <victororozcor@hotmail.com>
2020-02-27 16:36:43 -0600

    Data type operators
    Added example for data types

---
+++ src/03-data-type-operators.js
@@ -0,0 +1,35 @@
+/*
+Data types and operators
+*/
+
+var num1 = 5;
+var num2 = 7;
+
+var operations;
+
+operations = num1 + num2;
+//operations=num1-num2;
+//operations=num1*num2;
+//operations=num1/num2;
+//operations=num1%num2;
+
+//Data types
+var number = 40;
+var textChain = "Text";
+var boolean = true; //false
+var nullValue = null;
+var undefinedVar;
+var object = [
+  //JSON JavaScriptObjectNotation
+  {
+    id: "1",
+    name: "victor"
+  }
+];
+//with typeof method you can know the type of the variable that you send
+console.log(typeof number);
+console.log(typeof textChain);
+console.log(typeof boolean);
+console.log(typeof nullValue);
+console.log(typeof undefinedVar);
+console.log(typeof object);
