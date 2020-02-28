c760ea60e8b950358657afa72ed275b15a4f291d
Yuushaa <victororozcor@hotmail.com>
2020-02-28 16:46:47 -0600

    control structures
    file using control structures

---
+++ src/04-control-structures.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//if-elseif-else condition
+/**Relational operators
+  > more than
+  <less than
+  >= more/equal
+  <= less/equal
+  == equal
+  != different
+  === equal (value & type)
+     */
+
+/**Logic operators
+  && and
+  || or
+  ! negative
+
+ */
+var age = 25;
+var name = "someName";
+
+if (age > 20) {
+  alert("if");
+}
+else if (<20){
+  alert("alse if")
+}
+else{
+  alert("else")
+}
